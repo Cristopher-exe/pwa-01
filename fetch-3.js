017ff9797c9bca71fdf6d32c651bc5e721e22658
cristopher-exe <cristopher.txt@gmail.com>
2022-10-04 21:39:24 -0600

    se agrego fetch-3.js

---
+++ fetch-3.js
@@ -0,0 +1,18 @@
+let usuario = {
+    nombre: 'Aitana',
+    edad: 25
+}
+
+fetch('https://reqres.in/', {
+    method: 'POST',
+    body: JSON.stringify(usuario), //data a enviar
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+.then(resp => resp.json())
+.then(console.log)
+.catch(error => {
+    console.log('Error en la peticion API POST');
+    console.log(error);
+});
